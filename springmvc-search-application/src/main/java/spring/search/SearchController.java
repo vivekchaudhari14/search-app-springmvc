a2bd37e387cf9db5d3e151afa54fc6bd1fd96fdc
vivekchaudhari14 <vivekchaudhari14@zohomail.in>
2026-06-22 11:40:30 +0530

    Add Controller

---
+++ springmvc-search-application/src/main/java/spring/search/SearchController.java
@@ -0,0 +1,14 @@
+package spring.search;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SearchController {
+	
+	@RequestMapping("/home")
+	public String home() {
+		return "home";
+	}
+}
